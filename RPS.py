3fa16f1ae4ab1b796a5ff2bf9782284499acfff9
Chinonso Nwakudu <nwakudue@gmail.com>
2024-08-31 19:27:21 +0300

    updated and made correction by adding name value to Enum instead of using .replace()

--- RPS.py
+++ RPS.py
@@ -7,27 +7,32 @@ class RPS(Enum):
     PAPER =2
     SCISSORS = 3
 
-print("")
-nonsoschoice = input("please enter....\n\n1 for Rock,\n2 for Paper, or\n3 for Scissors\n\n")
+print("\nWelcome to Rock-Paper-Scissors")
 
-nonso = int(nonsoschoice)
+# The users input options
+nonsos_choice = input("please enter....\n\n1 for Rock,\n2 for Paper, or\n3 for Scissors\n\n")
+nonso = int(nonsos_choice)
 
+# Ensuring the user enters the right options
 if nonso < 1 or nonso >3:
    sys.exit("you can only enter 1, 2, or 3")
 
-computerschoice = random.choice('123')
+# computers choice
+computerschoice = random.choice([1, 2, 3])
 computer = int(computerschoice)
 
-print("you chose" + " " + str((RPS(nonso))).replace('RPS.', "  ") + ".")
-print("computer chose" + " " + str((RPS(computer))).replace('RPS.', " ") + ".")
+# prints the result of the user and computer's choice
+print("you chose" + " " + RPS(nonso).name + ".")
+print("computer chose" + " " + RPS(computer).name + ".")
 
-if "nonso = 1 and computer = 2":
+#Determines the winner
+if nonso == 1 and computer == 2:
   print("yay!, nonso won🥇")
-elif "nonso = 3 and computer = 2":
+elif nonso == 3 and computer == 2:
    print("congratulations nonso on your win!🎉") 
-elif "nonso = 1 and computer = 3":
+elif nonso == 1 and computer == 3:
   print("Nonso won again!🥳")
-elif "nonso = computer":
+elif nonso == computer:
   print("We have a tie!")
 else:
   print("🐍 computer wins!")
